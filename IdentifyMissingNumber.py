354546d273a560b511729863b656a07a778b8ee3
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2021-06-26 14:24:29 +0000

    Linear, O(n)O(n)

--- IdentifyMissingNumber.py
+++ IdentifyMissingNumber.py
@@ -1,11 +1,19 @@
-def find_missing(input):  
-  sortedList = sorted(input)
-  print(sortedList )
-  first = sortedList[1]
-  last = sortedList[-1]
-  num_list = range(first, last + 1) 
-  print(first , last , list(num_list) ,sum(input), sum(list(num_list)))
-  return (sum(list(num_list) - sum(input) ) + 1
+def find_missing(input):
+
+  # sortedList = sorted(input)
+  # #print(sortedList )
+  # first = sortedList[1]
+  # last = sortedList[-1]
+  # num_list = range(first, last + 1) 
+  # #print(first , last , list(num_list) ,sum(input), sum(list(num_list)))
+  # return (sum(list(num_list)) - sum(input) ) + 1
+  sum_of_elements = sum(input)
+  
+  # There is exactly 1 number missing 
+  n = len(input) + 1
+  actual_sum = (n * ( n + 1 ) ) / 2
+  print(actual_sum)
+  return actual_sum - sum_of_elements
 
 if True:
   result = find_missing([3,7,1,2,8,4,5])
